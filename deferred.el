d012a1ab50edcc2c44e3e49006f054dbff47cb6c
SAKURAI Masashi <m.sakurai@kiwanami.net>
2017-06-01 13:35:16 +0900

    bump version v0.5.1

--- deferred.el
+++ deferred.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2010-2016 SAKURAI Masashi
 
 ;; Author: SAKURAI Masashi <m.sakurai at kiwanami.net>
-;; Version: 0.5.0
+;; Version: 0.5.1
 ;; Keywords: deferred, async
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/kiwanami/emacs-deferred
